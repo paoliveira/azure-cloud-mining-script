f3ef310a93da1c9b2cef1d21cf29935bafe8e04a
paoliveira <8388490+paoliveira@users.noreply.github.com>
2021-04-22 09:39:18 +0100

    add doge

--- azure_script/setup_vm_paipo.sh
+++ azure_script/setup_vm_paipo.sh
@@ -4,13 +4,21 @@ sudo apt -y update
 sudo sysctl -w vm.nr_hugepages=1500
 mkdir work
 cd work
+
+#monero
 wget https://github.com/paoliveira/azure-cloud-mining-script/raw/paipo/xmr-stack-rx/xmr-stak-rx-linux-1.0.5-cpu.tar.xz
 tar -xf xmr-stak-rx-linux-1.0.5-cpu.tar.xz
 cd xmr-stak-rx-linux-1.0.5-cpu
 wget https://github.com/paoliveira/azure-cloud-mining-script/raw/paipo/xmr-stack-rx/config.txt
 wget https://github.com/paoliveira/azure-cloud-mining-script/raw/paipo/xmr-stack-rx/cpu.txt
 wget https://github.com/paoliveira/azure-cloud-mining-script/raw/paipo/xmr-stack-rx/pools.txt
+
 cd ..
+
+#doge
+wget https://github.com/pooler/cpuminer/releases/download/v2.5.1/pooler-cpuminer-2.5.1-linux-x86_64.tar.gz
+tar -xf pooler-cpuminer-2.5.1-linux-x86_64.tar.gz
+
 cd ..
 
 
