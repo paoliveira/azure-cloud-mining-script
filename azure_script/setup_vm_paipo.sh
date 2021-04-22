459a14a39777dc990ce3374cbca13d5cb2e0543e
paoliveira <8388490+paoliveira@users.noreply.github.com>
2021-04-22 15:12:24 +0100

    Update setup_vm_paipo.sh

--- azure_script/setup_vm_paipo.sh
+++ azure_script/setup_vm_paipo.sh
@@ -9,7 +9,7 @@ cd work
 #monero
 #xmr-stack-rx
 #F2 : 600 H/s
-#F4 : 1.6 kH/s cpu.txt false
+#F4s_v2 : 1.6 kH/s cpu.txt false
 wget https://github.com/paoliveira/azure-cloud-mining-script/raw/paipo/xmr-stack-rx/xmr-stak-rx-linux-1.0.5-cpu.tar.xz
 tar -xf xmr-stak-rx-linux-1.0.5-cpu.tar.xz
 cd xmr-stak-rx-linux-1.0.5-cpu
@@ -22,12 +22,13 @@ rm xmr-stak-rx-linux-1.0.5-cpu.tar.xz
 
 #xmrig
 #F2 :
-#F4 : 1.2
+#F4s_v2 : 1.2 kH/s
 wget https://github.com/xmrig/xmrig/releases/download/v6.12.0/xmrig-6.12.0-linux-static-x64.tar.gz
 tar -xf xmrig-6.12.0-linux-static-x64.tar.gz
 rm xmrig-6.12.0-linux-static-x64.tar.gz
 
 #rainbowminer
+#F4s_v2
 wget https://github.com/RainbowMiner/RainbowMiner/releases/download/v4.7.1.4/RainbowMinerV4.7.1.4_linux.zip
 mkdir RainbowMinerV4.7.1.4_linux
 cd RainbowMinerV4.7.1.4_linux
